18d25e779ef1d7810fc36855c18b371fcb9f344e
Gilly <67492466+Gilly312@users.noreply.github.com>
2023-11-24 13:17:15 +0700

    test

--- Student.java
+++ Student.java
@@ -1,6 +1,6 @@
 package helloworld;
 
 public class Student {
-           private int studentID;
-           private String studentName;
+    private int studentID;
+    private String studentName;
 }
